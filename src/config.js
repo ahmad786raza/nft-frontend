f8408c1ba94d9be44006c4d9c53dcd45dc521181
erkrujjawal786 <kumarujjawal786@gmail.com>
2021-06-26 20:06:22 +0530

    changed contract add and api route

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    Contract_address : "0x024753c189c612bc7D1C5Ec64D442876b10Fca64",
+    Contract_address : "0x5bf153474CC7aF24af964c4190a1957cc046C5f3",
     Platform_address : "0x93b8d57D2CECdC0Fd485CFCD7fB965D575445DcB"
 }
                         
